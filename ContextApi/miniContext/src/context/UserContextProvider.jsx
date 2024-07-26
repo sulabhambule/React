d7ef21d96a99b02f3089d6af694d6bfb7fc1e271
Sulabh <sulabhambule@gmail.com>
2024-07-26 23:09:34 +0530

    contextApi

---
+++ ContextApi/miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import UserContext from "./UserContext.js";
+
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = React.useState();
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
