00bf600ea0bb5a88f0b8fc06e728b5ac7500167c
Sulabh <sulabhambule@gmail.com>
2024-07-26 14:58:21 +0530

    React Done

---
+++ router/src/components/User/User.jsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+const User = () => {
+  const { id } = useParams();
+  return <div className="bg-grey-600 text-white text-3xl p-4">User: {id}</div>;
+};
+
+export default User;
+  
