23a488b3e73bca877135c713f7940db397b900a0
Sulabh <sulabhambule@gmail.com>
2024-07-31 00:18:38 +0530

    Done

---
+++ MegaBlog/src/conf/conf.js
@@ -0,0 +1,10 @@
+import { FaApper } from "react-icons/fa";
+
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+  appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
+};
+
+export default conf;
